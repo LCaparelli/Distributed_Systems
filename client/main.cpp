57d10837e842edbb9f6df91fcb46e044f6a4bd25
lcaparel <lucas.caparelli@usp.br>
2019-05-17 22:28:35 -0300

    Add incorrect usage message

--- client/main.cpp
+++ client/main.cpp
@@ -11,7 +11,6 @@ int main() {
     auto request = getRequest();
 
     client_socket << request;
-
     client_socket >> request;
 
     std::cout << request << std::endl;
@@ -30,7 +29,7 @@ std::string getRequest() {
 
         request = "{\"operation\":\"" + operation +
                   "\",\"id\":" + id + "}";
-    } else {
+    } else if(operation == "POST"){
         std::getline(std::cin, name);
         std::getline(std::cin, address);
         std::getline(std::cin, balance);
@@ -40,6 +39,9 @@ std::string getRequest() {
                   "\",\"address\":\"" + address +
                   "\", \"balance\":" + balance +
                   "}";
+    } else {
+        std::cout << "Uso indevido. Operações permitidas:\nGET\n<id>\n************\nPOST\n<nome>\n<endereço>\n<saldo>\n";
+        exit(1);
     }
 
     return request;
