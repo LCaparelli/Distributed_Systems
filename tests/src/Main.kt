baa131310da0f25b586fcac3be6e96bab5104869
lcaparel <lucas.caparelli@usp.br>
2019-05-31 19:30:32 -0300

    Add tests

---
+++ tests/src/Main.kt
@@ -0,0 +1,66 @@
+import java.io.BufferedReader
+import java.io.BufferedWriter
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+import java.net.InetAddress
+import java.net.Socket
+import java.util.*
+import kotlin.concurrent.thread
+
+
+fun main(args: Array<String>) {
+    for (i in 0..999L) {
+        thread(start = true) {
+            sendRequest(Request(
+                    operation = "POST",
+                    name = UUID.randomUUID().toString(),
+                    address = UUID.randomUUID().toString(),
+                    balance = Random().nextFloat())
+            )
+        }
+        Thread.sleep(10)
+    }
+}
+
+
+private fun sendRequest(request: Request) {
+    try {
+        with(setUpSocket()) {
+            val writer = setUpBufferedWriterAndSendRequest(this, request)
+            val reader = BufferedReader(InputStreamReader(this.getInputStream()))
+
+            reader.forEachLine { println(it) }
+
+            writer.close()
+            reader.close()
+        }
+    } catch (e: Exception) {
+        e.printStackTrace()
+    }
+}
+
+
+private fun setUpBufferedWriterAndSendRequest(socket: Socket, request: Request) =
+        BufferedWriter(OutputStreamWriter(socket.getOutputStream(), "UTF8")).also {
+            it.write(
+                    """{"operation":"${request.operation}","name":"${request.name}","address":"${request.address}", "balance":${request.balance}}"""
+            )
+            it.flush()
+        }
+
+
+private fun setUpSocket(): Socket {
+    val hostname = "localhost"
+    val port = 8080
+
+    val addr = InetAddress.getByName(hostname)
+    return Socket(addr, port)
+}
+
+
+data class Request(
+        val operation: String,
+        val name: String,
+        val address: String,
+        val balance: Float
+)
